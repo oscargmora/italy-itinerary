9a802ac3269961de343d35bcb02b63c3f7afee0c
Oscar Mora <oscgmora@gmail.com>
2023-05-04 23:22:25 -0400

    Create Home Page
    This commmit creates the homepage for the website. It also changes the website from a restaruant page to an Italy itinerary. This adds a background image that changes for each new tab that is clicked on.

--- src/homePage.js
+++ src/homePage.js
@@ -1,13 +1,39 @@
 const parent = document.querySelector('#content');
 
 const createHomePage = () => {
+    document.body.style.backgroundImage = "url('/src/img/background-img.jpeg')";
+
     const div = document.createElement('div');
-    div.classList = 'div';
+    div.classList.add('div');
+    div.setAttribute('id', 'main-div');
     parent.appendChild(div);
 
-    const detail = document.createElement('p');
-    detail.innerText = 'Delicious Food';
-    div.appendChild(detail);
+    const mainContainer = document.createElement('div');
+    mainContainer.classList.add('main-container');
+    div.appendChild(mainContainer);
+
+    const mainTitle = document.createElement('p');
+    mainTitle.innerText = "Bella Ciao! Pronto per l'italia?";
+    mainTitle.setAttribute('id', 'main-title');
+    mainContainer.appendChild(mainTitle);
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+    div.appendChild(imgContainer);
+
+    const img = document.createElement('img');
+    img.src = '/src/img/postcard-img.png';
+    img.setAttribute('id', 'main-img');
+    imgContainer.appendChild(img);
+
+    const subContainer = document.createElement('div');
+    subContainer.classList.add('sub-container');
+    div.appendChild(subContainer);
+
+    const subTitle = document.createElement('p');
+    subTitle.innerText = 'Choose a Selection Above!';
+    subTitle.setAttribute('id', 'sub-title');
+    subContainer.appendChild(subTitle);
 };
 
 export default createHomePage;
